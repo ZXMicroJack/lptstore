92f5150a76f9ee3d9655c0f320efe9d3c915f65f
MicroJack <zx.micro.jack@gmail.com>
2023-01-31 15:52:31 +0000

    handshakes with byte reading.

--- pico/lptstore.c
+++ pico/lptstore.c
@@ -8,9 +8,7 @@
 
 #include "pico/bootrom.h"
 #include "pico/stdlib.h"
-// #include "pico/audio_i2s.h"
 #include "pico/binary_info.h"
-// #include "pico/multicore.h"
 #include "pico/bootrom.h"
 
 #ifdef DEBUG
@@ -36,9 +34,11 @@ int readbyte() {
   while (((c = readlpt()) & 0x10) == 0x00)
     tight_loop_contents();
   d = (c & 0xf) << 4;
+  gpio_put(19, 1);
   while (((c = readlpt()) & 0x10) == 0x10)
     tight_loop_contents();
   d |= (c & 0xf);
+  gpio_put(19, 0);
   return d;
 }
 
@@ -48,13 +48,15 @@ void writebyte(uint8_t b) {
   gpio_put(18, (b & 0x40) ? 0 : 1);
   gpio_put(20, (b & 0x80) ? 0 : 1);
   gpio_put(19, 1);
-  sleep_ms(10);
+  while ((readlpt() & 0x10) == 0x00)
+    tight_loop_contents();
   gpio_put(16, (b & 0x01) ? 0 : 1);
   gpio_put(17, (b & 0x02) ? 0 : 1);
   gpio_put(18, (b & 0x04) ? 0 : 1);
   gpio_put(20, (b & 0x08) ? 0 : 1);
   gpio_put(19, 0);
-  sleep_ms(10);
+  while ((readlpt() & 0x10) == 0x10)
+    tight_loop_contents();
 }
 
 int main()
@@ -110,11 +112,9 @@ int main()
   
   for(;;) {
     int c = getchar_timeout_us(0);
+    if (c == 'h') printf("(h)elp (q)uit (r)eadgpios (w)read (e)mitbyte (d)readgpios (f)writegpios\n");
     if (c == 'q') break;
     if (c == 'r') {
-//       printf("gpios: %d%d%d%d%d\n", gpio_get(21), gpio_get(22), gpio_get(26), gpio_get(27), gpio_get(28));
-//       printf("gpios: %d%d%d%d%d\n", gpio_get(GPIO_21), gpio_get(22), gpio_get(26), gpio_get(27), gpio_get(28));
-      
       uint32_t dw = gpio_get_all() >> 20;
       uint8_t b = (dw >> 8) | ((dw&0x80) >> 6) | ((dw&0x40) >> 4) | ((dw&0x04) << 1) | ((dw&0x02) << 3);
 
@@ -133,22 +133,8 @@ int main()
     }
     
     if (c == 'd') {
-      printf("w:%d\n", w);
-      gpio_put(16, w & 1);
-      gpio_put(17, !(w & 1));
-      gpio_put(18, w & 1);
-      gpio_put(19, !(w & 1));
-      gpio_put(20, w & 1);
-      w ++;
+      printf("in:%02X\n", readlpt());
     }
-    
-#if 0
-    if (c >= '0' && c <= '4') {
-      printf("%c = %d\n", c, s[c - '0']);
-      gpio_put(16 + c - '0', s[c - '0']);
-      s[c - '0'] = s[c - '0'] ? 0 : 1;
-    }
-#endif
     
     if (c == 'f') {
       printf("f:%02X\n", b);
@@ -160,26 +146,7 @@ int main()
       b <<= 1;
       if (b == 0x20) b = 1;
     }
-    if (c == 'b') {
-      // working very well - TODO optimise
-      printf("b:%02X\n", b);
-      gpio_put(16, (b & 0x10) ? 0 : 1);
-      gpio_put(17, (b & 0x20) ? 0 : 1);
-      gpio_put(18, (b & 0x40) ? 0 : 1);
-      gpio_put(20, (b & 0x80) ? 0 : 1);
-      gpio_put(19, 1);
-      sleep_ms(10);
-      gpio_put(16, (b & 0x01) ? 0 : 1);
-      gpio_put(17, (b & 0x02) ? 0 : 1);
-      gpio_put(18, (b & 0x04) ? 0 : 1);
-      gpio_put(20, (b & 0x08) ? 0 : 1);
-      gpio_put(19, 0);
-      sleep_ms(10);
-      
-      b <<= 1;
-      if (!b) b = 1;
-      printf("done\n");
-    }
+
   }
   
 // 	wtsynth_Kill();
