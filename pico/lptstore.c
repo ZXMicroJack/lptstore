c1a6e3e937e409d809518ba5f58779d46b923936
MicroJack <zx.micro.jack@gmail.com>
2023-02-05 09:43:15 +0000

    Stalls randomly in response to different timings on receive end.

--- pico/lptstore.c
+++ pico/lptstore.c
@@ -29,17 +29,55 @@ int readlpt() {
   return b;
 }
 
+int hasbyte() {
+  return ((readlpt() & 0x10) == 0x10);
+}
+
 int readbyte() {
   int c, d;
+//   printf("a");
   while (((c = readlpt()) & 0x10) == 0x00)
     tight_loop_contents();
   d = (c & 0xf) << 4;
+//   printf("b");
+  gpio_put(19, 1);
+//   printf("c");
+  while (((c = readlpt()) & 0x10) == 0x10)
+    tight_loop_contents();
+//   printf("d");
+  d |= (c & 0xf);
+  gpio_put(19, 0);
+
+  // extra handshake
+  while (((c = readlpt()) & 0x10) == 0x00)
+    tight_loop_contents();
   gpio_put(19, 1);
   while (((c = readlpt()) & 0x10) == 0x10)
     tight_loop_contents();
-  d |= (c & 0xf);
   gpio_put(19, 0);
+  
+
+
+  // if this is removed then the PC doesn't seem to register the last
+  // port waggle.
+//   sleep_ms(2);
+  
+  
+//   while (((c = readlpt()) & 0x10) == 0x00)
+//     tight_loop_contents();
+//   while (((c = readlpt()) & 0x10) == 0x10)
+//     tight_loop_contents();
+
+  //   printf("e");
   return d;
+}
+
+int readbytes(uint8_t *buff, int len) {
+  int i;
+  for (i=0; i<len; i++) {
+    buff[i] = readbyte();
+  }
+  return 0;
 }
 
 void writebyte(uint8_t b) {
@@ -57,6 +95,29 @@ void writebyte(uint8_t b) {
   gpio_put(19, 0);
   while ((readlpt() & 0x10) == 0x10)
     tight_loop_contents();
+}
+
+void writebytes(uint8_t *d, int len) {
+  int i;
+  for (i=0; i<len; i++) {
+    writebyte(d[i]);
+  }
+}
+
+int init_disk(void) {
+  return 0;
+}
+uint32_t get_disks(void) { return 0xffff; }
+uint32_t get_disk_blocks(uint8_t disk) {
+  return 32*1024*2;
+}
+
+int read_disk_sector(uint8_t disk, int sector, uint8_t *buff) {
+  return 0;
+}
+
+int write_disk_sector(uint8_t disk, int sector, uint8_t *buff) {
+  return 0;
 }
 
 int main()
@@ -146,7 +207,84 @@ int main()
       b <<= 1;
       if (b == 0x20) b = 1;
     }
+    
+    if (c == 'p') {
+      // protocol mode
+      int quit = 0;
+      printf("Entering protocol mode.\n");
+      while (!quit) {
+        uint8_t buff[512];
+#define LPTS_HELLO            0x00
+#define LPTS_QUERY_DISKS      0x01
+#define LPTS_QUERY_DISK_CAPS  0x02
+#define LPTS_READ_BLOCK       0x03
+#define LPTS_WRITE_BLOCK      0x04
+        
+#define LPTS_VERSION_MAJOR    0
+#define LPTS_VERSION_MINOR    1
 
+        int c = getchar_timeout_us(0);
+        if (c == 'q') break;
+        if (hasbyte()) {
+          uint8_t cmd = readbyte();
+          switch(cmd) {
+            case LPTS_HELLO:
+              buff[0] = LPTS_VERSION_MAJOR;
+              buff[1] = LPTS_VERSION_MINOR;
+              writebytes(buff, 2);
+              break;
+            case LPTS_QUERY_DISKS: {
+              uint32_t w = get_disks();
+              buff[0] = w >> 24;
+              buff[1] = (w >> 16) & 0xff;
+              buff[2] = (w >> 8) & 0xff;
+              buff[3] = w  & 0xff;
+              writebytes(buff, 4);
+              break;
+            }
+            case LPTS_QUERY_DISK_CAPS: {
+              uint32_t w;
+              readbytes(buff, 1);
+              w = get_disk_blocks(buff[0]);
+              buff[0] = w >> 24;
+              buff[1] = (w >> 16) & 0xff;
+              buff[2] = (w >> 8) & 0xff;
+              buff[3] = w  & 0xff;
+              writebytes(buff, 4);
+              break;
+            }
+            case LPTS_READ_BLOCK: {
+              readbytes(buff, 5);
+              
+              uint8_t drv = buff[0];
+              uint32_t lba = (buff[1] << 24) | (buff[2] << 16) | (buff[3] << 8) | buff[4];
+              uint8_t r = read_disk_sector(drv, lba, buff);
+              
+              writebyte(r);
+              if (!r) {
+                writebytes(buff, 512);
+              }
+              break;
+            }
+            case LPTS_WRITE_BLOCK: {
+              readbytes(buff, 5);
+              
+              uint8_t drv = buff[0];
+              uint32_t lba = (buff[1] << 24) | (buff[2] << 16) | (buff[3] << 8) | buff[4];
+              
+              readbytes(buff, 512);
+              uint8_t r = write_disk_sector(drv, lba, buff);
+              
+              writebyte(r);
+              break;
+            }
+              
+          }
+          
+        }
+      }
+      printf("Leaving protocol mode.\n");
+    }
   }
   
 // 	wtsynth_Kill();
